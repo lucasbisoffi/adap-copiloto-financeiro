b1f27c2508033d9b01e787d25e1461837c2b5c9a
Lucas Bisoffi <lucasdb3301@gmail.com>
2025-07-09 14:30:28 -0300

    Fix: categoria 'particular' adicionada

--- src/models/Income.js
+++ src/models/Income.js
@@ -15,7 +15,7 @@ const incomeSchema = new mongoose.Schema({
   source: {
     type: String,
     required: true,
-    enum: ['Uber', '99', 'InDrive', 'UBRA', 'Garupa', 'Rota 77', 'Guri', 'Mais Próximo', 'Outros'],
+    enum: ['Uber', '99', 'InDrive', 'UBRA', 'Garupa', 'Rota 77', 'Guri', 'Mais Próximo', 'Particular', 'Outros'],
     default: 'Outros'
   },
   // CAMPO OPCIONAL: Taxa do aplicativo, caso o motorista informe.
